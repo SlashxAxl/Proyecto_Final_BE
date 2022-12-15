dbf6d1069b7eab113d754bc46d7901352748ec4e
danielmtz <martinezdaniel554@gmail.com>
2022-12-15 09:25:59 -0600

    agregando elementos

--- Proyecto Final/src/server.js
+++ Proyecto Final/src/server.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import  ProductManager from "./ProductManager.json" 
 
 const app = express();
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
 const port = 8080;
+const productManager = new ProductManager();
+
+app.get('/',(req,res)=>{
+    res.json(productManager);
+})
 
 app.listen(port,()=>{
     console.log('Listening on port ' + port)
